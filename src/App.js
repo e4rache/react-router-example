04903e75898af7aee1415952c2b6a8cdc4cc7ccf
e4rache <e4rache@protonmail.com>
2018-07-30 19:01:17 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+class App extends Component {
+  render() {
+    console.log("hello there");
+    return <div className="App" />;
+  }
+}
+
+export default App;
